a368e9710b744359bdbdf36d8b3085c31bd99e77
徐玉祥 <xuyuxiang29@foxmail.com>
2020-07-20 11:28:47 +0800

    更新鉴权异常枚举

--- guns-base-support/guns-core/src/main/java/cn/stylefeng/guns/core/exception/enums/AuthExceptionEnum.java
+++ guns-base-support/guns-core/src/main/java/cn/stylefeng/guns/core/exception/enums/AuthExceptionEnum.java
@@ -54,44 +54,39 @@ public enum AuthExceptionEnum implements AbstractBaseExceptionEnum {
     ACCOUNT_PWD_ERROR(2, "账号或密码错误，请检查account或password参数"),
 
     /**
-     * 用户盐值为空
-     */
-    USER_SALT_EMPTY(3, "用户盐值为空，请联系管理员"),
-
-    /**
      * 验证码错误
      */
-    VALID_CODE_ERROR(4, "验证码错误，请检查captcha参数"),
+    VALID_CODE_ERROR(3, "验证码错误，请检查captcha参数"),
 
     /**
      * 请求token为空
      */
-    REQUEST_TOKEN_EMPTY(5, "请求token为空，请携带token访问本接口"),
+    REQUEST_TOKEN_EMPTY(4, "请求token为空，请携带token访问本接口"),
 
     /**
      * token格式不正确，token请以Bearer开头
      */
-    NOT_VALID_TOKEN_TYPE(6, "token格式不正确，token请以Bearer开头，并且Bearer后边带一个空格"),
+    NOT_VALID_TOKEN_TYPE(5, "token格式不正确，token请以Bearer开头，并且Bearer后边带一个空格"),
 
     /**
      * 请求token错误
      */
-    REQUEST_TOKEN_ERROR(7, "请求token错误"),
+    REQUEST_TOKEN_ERROR(6, "请求token错误"),
 
     /**
      * 账号被冻结
      */
-    ACCOUNT_FREEZE_ERROR(8, "账号被冻结，请联系管理员"),
+    ACCOUNT_FREEZE_ERROR(7, "账号被冻结，请联系管理员"),
 
     /**
      * 登录已过期
      */
-    LOGIN_EXPIRED(9, "登录已过期，请重新登录"),
+    LOGIN_EXPIRED(8, "登录已过期，请重新登录"),
 
     /**
      * 无登录用户
      */
-    NO_LOGIN_USER(10, "无登录用户");
+    NO_LOGIN_USER(9, "无登录用户");
 
     private final Integer code;
 
